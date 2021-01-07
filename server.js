63d2dabf460da764192714294871e73f9b55ff82
Lauren Suniga <laurensuniga@Laurens-MacBook-Pro.local>
2021-01-06 18:46:17 -0800

    installs and configs cors

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const logger = require('morgan');
+const cors = require('cors');
 
 
 require('dotenv').config();
@@ -9,11 +10,10 @@ const app = express();
 
 app.use(express.json());
 app.use(logger('dev'));
+app.use(cors());
 
-// app.use(corse());  => will still need to install this later
 
-
-// app.use('/api/users', require('./routes/api/users'));  // can put an in-line require statement
+app.use('/api/users', require('./routes/api/users'));  // can put an in-line require statement
 
 const port = process.env.PORT || 3001;
 
